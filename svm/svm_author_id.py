6327a05c7b0edaa68a0647f2b9c37b231f0886a9
FMurry <frederic.wesley.murry@gmail.com>
2016-12-05 23:54:01 -0800

    Finished SVM

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -6,12 +6,14 @@
     Use a SVM to identify emails from the Enron corpus by their authors:    
     Sara has label 0
     Chris has label 1
-"""
-    
+"""   
+
 import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+import matplotlib.pyplot as plt
+from sklearn import svm
 
 
 ### features_train and features_test are the features for the training
@@ -21,14 +23,21 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 
-
 #########################################################
 ### your code goes here ###
 
 #########################################################
 
-from sklearn import svm
-clf = svm.SVC(kernel = "linear", gamma = 1.0)
+clf = svm.SVC(C=10000,kernel = "rbf")
+#features_train = features_train[:len(features_train)/100] 
+#labels_train = labels_train[:len(labels_train)/100] 
+initial_time = time()
 clf.fit(features_train, labels_train)
-prettyPicture(clf,features_test,labels_test)
-plt.show()
+print "Training time is:", time()-initial_time
+print "Accuracy is:", clf.score(features_test,labels_test) 
+i = 0
+count = 0
+for feature in features_test:
+	if clf.predict(feature) == 1:
+		count+=1
+print count
